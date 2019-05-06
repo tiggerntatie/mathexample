c3de7e365b5abf8d3c5c4dfe1b5ec0c8343fae90
Eric Dennison <ericd@netdenizen.com>
2019-05-06 10:29:11 -0400

    Updated from Brython Server: 5/6/2019 10:29:11 AM

--- mathexample.py
+++ mathexample.py
@@ -1,1 +1,21 @@
 # mathexample
+from browser import open
+from random import randint
+from math import sqrt
+
+
+class VectorMagnitudeExample:
+    
+    question = "Compute the magnitude of the vector, <{0},{1},{2}>:"
+    
+    def __init__(self):
+        self.a = randint(2,8)
+        self.b = randint(2,8)
+        self.c = randint(2,8)
+    
+    @property
+    def correctAnswer(self):
+        return sqrt(self.a**2 + self.b**2 + self.c**2)
+        
+    
+    
